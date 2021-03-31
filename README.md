6dd7db19054e5212554fc3ef967fffdda62cc84e
alexandre.giron <aagiron@aagiron>
2021-03-31 10:13:55 -0300

    Keccak_256 LSB and update README

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# hash-lsb-generators
-A repository for the implementation that outputs the Least Significant Bit (LSB) from hashes
+# Hash-LSB
+
+A repository for the implementations that outputs the Least Significant Bit (LSB) from hashes.
+
+Hash functions/Standards currently implemented: `keccak_256`.
+
+## How can I use it?
+
+Change the paths in the code where you download the blockchain. Run it with: `python3 name.py`, where `name.py` is the desired script file Ex: `python3 keccakgen.py`.
+
+
---
+++ keccakgen.py
@@ -0,0 +1,47 @@
+import sys
+import os
+from sha3 import keccak_256
+
+#Configure here
+ENTROPY_SIZE = 32 #gathered from linux urandom
+SEED_THRESHOLD = 1048576 #update seed
+OUTPUT_SIZE = 1000000000 #amount of LSBits
+OUTPUT_NAME = "keccak256_LSB.data"
+
+def savePart(data):
+	arq = open(OUTPUT_NAME, 'ab')
+	arq.write(data.to_bytes(1, byteorder = 'little'))			
+	arq.close()
+
+def runGenerator(limitMBits):
+
+	hashed = os.urandom(ENTROPY_SIZE)
+	byteOutput = 0
+	countBits = 0
+	calls = 0
+	for i in range(limitMBits):		
+		calls = calls + 1
+		hashed = keccak_256(hashed).digest() #seems to be a string output
+
+		LSB = hashed[-1] & 1
+		if (LSB == 1):
+			byteOutput = (byteOutput << 1)+1
+		else:
+			byteOutput = (byteOutput << 1)
+
+		countBits = countBits + 1
+		if (countBits == 8):
+			#save this in respective file
+			savePart(byteOutput)
+			byteOutput = 0
+			countBits = 0
+		
+		if calls == SEED_THRESHOLD:			
+			hashed = os.urandom(ENTROPY_SIZE)
+			calls = 0
+	
+if __name__ == "__main__":
+	print("Keccak 256 Generator starting...")
+	runGenerator(OUTPUT_SIZE) #1 Gbit
+
+	print("Ended.")
